cc14734d8725402c27fb959e9179595734f17f99
BaoNguyen <baonngforwork@gmail.com>
2025-12-24 16:04:45 +0700

    Bao Dep Zai VL

--- resident_management/apps/contract/urls.py
+++ resident_management/apps/contract/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import
+from .views import ContractViewSet
 
 router = DefaultRouter()
-urlpatterns = router.urls
+router.register(r'', ContractViewSet, basename='contract')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
